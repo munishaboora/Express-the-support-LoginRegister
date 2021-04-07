360112581e4391dcadb228c4e4dee5af72208c42
munishaboora <78922862+munishaboora@users.noreply.github.com>
2021-04-07 20:01:36 +0100

    All the code

--- config/passport.js
+++ config/passport.js
@@ -6,15 +6,37 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports = function (passport) {
-    passport.use(passport
-        new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    passport.use(passport)
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
         // Match User
         User.fineOne({ email: email })
             .then(user => {
-                if ()
+                if (!User) {
+                    return done(null, false, { message: 'Email is not registered' });
+                }
 
+                // Match password
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) throw err;
+
+                    if (isMatch) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, { message: 'Password entered is incorrect' })
+                    }
+                });
             })
             .catch(err => console.log(err));
     })
-    );
+        ;
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
+// Welcome Page
 router.get('/', (req, res) => res.render('Welcome'));
+//Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('Dashboard', {
+        name: req.user.name
+    }));
 
 module.exports = router;
